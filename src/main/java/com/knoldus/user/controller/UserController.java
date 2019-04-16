4368cbf30676c957cefe427abe961b2a1341df82
shivangi Gupta <shivangi.gupta@knoldus.in>
2019-04-16 16:51:08 +0530

    Added update user method and one method with custom query

--- src/main/java/com/knoldus/user/controller/UserController.java
+++ src/main/java/com/knoldus/user/controller/UserController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -31,18 +33,36 @@ public class UserController {
     
     @PostMapping("/user/add")
     public User addUser(@RequestBody User newUser) {
-        User bob = User.builder().id(newUser.getId()).name(newUser.getName()).age(newUser.getAge()).build();
+        User bob = User.builder()
+                .id(newUser.getId())
+                .name(newUser.getName())
+                .age(newUser.getAge())
+                .build();
         return repository.save(bob);
     }
     
-    /*@PutMapping("/user/update")
-    public User updateUser(@RequestBody User updatedUser, @PathVariable String id) {
-        return repository.save(updatedUser);
-    }*/
+    @PutMapping("/user/update")
+    public Optional<User> updateUser(@RequestBody User updatedUser, @PathVariable String id) {
+        Optional<User> existingUser = repository.findById(id);
+        if (existingUser.isPresent()) {
+            User newUser = repository.save(User.builder()
+                    .id(updatedUser.getId())
+                    .name(updatedUser.getName())
+                    .age(updatedUser.getAge())
+                    .build());
+            return Optional.of(newUser);
+        }
+        return existingUser;
+    }
     
     @DeleteMapping("/user/delete")
     public String deleteUser(@PathVariable String idToBeDeleted) {
         repository.deleteById(idToBeDeleted);
         return "User deleted";
+    }
+    
+    @GetMapping("/user/getbynameandage/{name}/{age}")
+    public List<User> getUserByTwoParams(@PathVariable String name, String age) {
+        return repository.getUserByNameAndAge(name, age);
     }
 }
--- src/main/java/com/knoldus/user/repository/UserRepository.java
+++ src/main/java/com/knoldus/user/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package com.knoldus.user.repository;
 
 import com.knoldus.user.models.User;
+import org.springframework.data.cassandra.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * CRUD operations for the user.
  */
 public interface UserRepository extends CrudRepository<User, String> {
+    
+    @Query(allowFiltering = true)
+    List<User> getUserByNameAndAge(String name, String age);
+ 
 }
