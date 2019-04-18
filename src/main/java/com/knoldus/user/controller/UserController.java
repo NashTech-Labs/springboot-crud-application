8cb6b0ce597ca5cfda5460eb36b862a506f1c834
shivangi Gupta <shivangi.gupta@knoldus.in>
2019-04-18 12:35:53 +0530

    Added javadoc.

--- src/main/java/com/knoldus/user/controller/UserController.java
+++ src/main/java/com/knoldus/user/controller/UserController.java
@@ -21,16 +21,33 @@ public class UserController {
     @Autowired
     private UserRepository repository;
     
+    /**
+     * Shows the welcome page.
+     *
+     * @return
+     */
     @RequestMapping("/")
     public String index() {
         return "Welcome to the CRUD application!!";
     }
     
+    /**
+     * Finds user by {@param id}
+     *
+     * @param id userId
+     * @return
+     */
     @GetMapping("/user/{id}")
     public Optional<User> getUser(@PathVariable String id) {
         return repository.findById(id);
     }
     
+    /**
+     * Adds a new user to the user table.
+     *
+     * @param newUser User to be added.
+     * @return
+     */
     @PostMapping("/user/add")
     public User addUser(@RequestBody User newUser) {
         User bob = User.builder()
@@ -41,6 +58,14 @@ public class UserController {
         return repository.save(bob);
     }
     
+    /**
+     * Update the existing user.
+     *
+     * @param id   user id to be updated.
+     * @param name updated name.
+     * @param age  updated age.
+     * @return
+     */
     @PutMapping("/user/update/{id}/{name}/{age}")
     public Optional<User> updateUser(@PathVariable String id, @PathVariable String name, @PathVariable String age) {
         Optional<User> existingUser = repository.findById(id);
@@ -55,6 +80,12 @@ public class UserController {
         return existingUser;
     }
     
+    /**
+     * Deletes a particular user.
+     *
+     * @param idToBeDeleted user id to be deleted.
+     * @return
+     */
     @DeleteMapping("/user/delete/{idToBeDeleted}")
     public String deleteUser(@PathVariable String idToBeDeleted) {
         Optional<User> existingUser = repository.findById(idToBeDeleted);
@@ -68,11 +99,24 @@ public class UserController {
         }
     }
     
+    /**
+     * Fetches user by the given name and age.
+     *
+     * @param name
+     * @param age
+     * @return
+     */
     @GetMapping("/user/getbynameandage/{name}/{age}")
     public List<User> getUserByTwoParams(@PathVariable String name, String age) {
         return repository.getUserByNameAndAge(name, age);
     }
     
+    /**
+     * counts number of user with a particular name.
+     *
+     * @param name
+     * @return
+     */
     @GetMapping("/user/count/{name}")
     public long countUsersByName(@PathVariable String name) {
         return repository.countByName(name);
